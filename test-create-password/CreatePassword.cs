9e0f130e2d80044438f5d44406281b41c9527f02
Hanife <hanife.sayilir@cfn.com.tr>
2023-02-27 09:41:41 +0300

    bug fix for all reply no

--- test-create-password/CreatePassword.cs
+++ test-create-password/CreatePassword.cs
@@ -15,35 +15,41 @@ namespace test_create_password
 {
     public class CreatePassword
     {
-        public int passwordLength = 0;
-        public string numberAllowed= string.Empty;
-        public string upperCaseAllowed = string.Empty;
-        public string lowerCaseAllowed = string.Empty;
-        public string specialCharacterAllowed = string.Empty;
-        public bool validNumber;
-        public bool validUpperCase;
-        public bool validLowerCase;
-        public bool validSpecialCharacterAllowed;
-        public bool validLength;
-        public List<string> password = new List<string>();
-        public string finalPassword = string.Empty;
+        private int passwordLength = 0;
+        private string numberAllowed= string.Empty;
+        private string upperCaseAllowed = string.Empty;
+        private string lowerCaseAllowed = string.Empty;
+        private string specialCharacterAllowed = string.Empty;
+        private bool anyValidInput = false;
+        private List<string> password = new List<string>();
+        private string finalPassword = string.Empty;
 
-        public static List<QuestionType> selectedList = new List<QuestionType>();
-        public static string questionNumber = "Do you want to include numbers?";
-        public static string questionLowerCase = "How about lowercase characters?";
-        public static string questionUpperCase = "OK! How about uppercase characters?";
-        public static string questionLength = "Great! Lastly. How long do you want to keep your password length?";
-        public static string questionSpecialCharacter = "All right! We are almost done. Would you also want to add special characters?";
-        public static string validityConsoleMessage = "Please enter a valid input. One of the following characters 'Y', 'y', 'n', 'N' are considered valid only.";
-        public static string validityLengthConsoleMessage = "Please enter a valid input. The length should be greater than 1.Please try Again.";
-        public static string wellcomeTitle = "Wellcome to the BESTPASSWORDMANAGER";
-        public static Random random = new Random();
-        public static List<string> numberList = new List<string>() { "0","1","2","3","4","5","6","7","8","9"};
-        public static List<string> lowerCaseList = new List<string>(){ "a", "b", "c", "d","e","f","g","h", "i", "j",  "k", "l", "m","n","o","p", "q","r", "s","t", "u", "v","w","x","y","z"};
-        public static List<string> upperCaseList = new List<string>(){ "A", "B", "C", "D","F","G","H","I", "J",  "K", "L", "M","N","O","P", "Q","R", "S","T", "U", "V","W","X","Y","Z"};
-        public static List<string> specialCharacterList = new List<string>() { "#", "$", ".", ",", "/",":", ";", "%","?"};
-       
-       public CreatePassword () 
+        private static List<QuestionType> selectedList = new List<QuestionType>();
+        private static string questionNumber = "Do you want to include numbers?";
+        private static string questionLowerCase = "How about lowercase characters?";
+        private static string questionUpperCase = "OK! How about uppercase characters?";
+        private static string questionLength = "Great! Lastly. How long do you want to keep your password length?";
+        private static string questionSpecialCharacter = "All right! We are almost done. Would you also want to add special characters?";
+        private static string validityConsoleMessage = "Please enter a valid input. One of the following characters 'Y', 'y', 'n', 'N' are considered valid only.";
+        private static string validityLengthConsoleMessage = "Please enter a valid input. The length should be greater than 1.Please try Again.";
+        private static string wellcomeTitle = "Wellcome to the BESTPASSWORDMANAGER";
+        private static Random random = new Random();
+        private static List<string> numberList = new List<string>() { "0","1","2","3","4","5","6","7","8","9"};
+        private static List<string> lowerCaseList = new List<string>(){ "a", "b", "c", "d","e","f","g","h", "i", "j",  "k", "l", "m","n","o","p", "q","r", "s","t", "u", "v","w","x","y","z"};
+        private static List<string> upperCaseList = new List<string>(){ "A", "B", "C", "D","F","G","H","I", "J",  "K", "L", "M","N","O","P", "Q","R", "S","T", "U", "V","W","X","Y","Z"};
+        private static List<string> specialCharacterList = new List<string>() { "#", "$", ".", ",", "/",":", ";", "%","?"};
+
+        public List<QuestionType> getSelectedList ()
+        {
+            return selectedList;
+        }
+
+        public int getPasswordLength()
+        {
+            return passwordLength;
+        }
+
+        public CreatePassword () 
             {
        
             WellcomeScreen();
@@ -73,19 +79,19 @@ namespace test_create_password
             switch (sentValue)
             {
                 case QuestionType.NUMBER: 
-                    CallQuestionAndReadFromConsole(sentValue, questionNumber, numberAllowed, validNumber);
+                    CallQuestionAndReadFromConsole(sentValue, questionNumber, numberAllowed);
                     break;
                 case QuestionType.LOWERCASE:
-                    CallQuestionAndReadFromConsole(sentValue, questionLowerCase, lowerCaseAllowed, validLowerCase);
+                    CallQuestionAndReadFromConsole(sentValue, questionLowerCase, lowerCaseAllowed);
                     break;
                 case QuestionType.UPPERCASE:
-                    CallQuestionAndReadFromConsole(sentValue, questionUpperCase, upperCaseAllowed, validUpperCase);
+                    CallQuestionAndReadFromConsole(sentValue, questionUpperCase, upperCaseAllowed);
                     break;
                 case QuestionType.SPECIALCHARACTER:
-                    CallQuestionAndReadFromConsole(sentValue, questionSpecialCharacter, specialCharacterAllowed, validSpecialCharacterAllowed);
+                    CallQuestionAndReadFromConsole(sentValue, questionSpecialCharacter, specialCharacterAllowed);
                     break;
                 case QuestionType.LENGTH:
-                    ReadLengthFromConsole(questionLength, validLength);
+                    ReadLengthFromConsole(questionLength);
                     break;
                 default:
                     Console.WriteLine("Invalid Type");
@@ -93,26 +99,31 @@ namespace test_create_password
             }
         }
           
-        public void CallQuestionAndReadFromConsole(QuestionType value,string question, string typeAllowed, bool validType)
+        public void CallQuestionAndReadFromConsole(QuestionType value,string question, string typeAllowed)
         {
             Console.WriteLine(question);
             typeAllowed = Console.ReadLine();
-            validType = CheckValidityOfInputs(typeAllowed);
+            bool validType = CheckValidityOfInputs(typeAllowed);
           
 
             if (!validType)
             {
                 Console.WriteLine(validityConsoleMessage);
-                CallQuestionAndReadFromConsole(value, question, typeAllowed, validType);
+                CallQuestionAndReadFromConsole(value, question, typeAllowed);
             } 
             else
             {
-                if (IsAllowed(typeAllowed)) selectedList.Add(value);
+                if (IsAllowed(typeAllowed)) 
+                {
+                    selectedList.Add(value);
+                    anyValidInput= true;
+                }
+                
             }
             
         }
 
-        public void ReadLengthFromConsole(string question, bool validType)
+        public void ReadLengthFromConsole(string question)
         {
             int number = 0;
             Console.WriteLine(question);
@@ -120,11 +131,11 @@ namespace test_create_password
 
             if (int.TryParse(temp, out number))
             {
-                validType = CheckValidityOfIntegerInput(number);
+               bool validType = CheckValidityOfIntegerInput(number);
                 if (!validType)
                 {
                     Console.WriteLine(validityLengthConsoleMessage);
-                    ReadLengthFromConsole(question, validType);
+                    ReadLengthFromConsole(question);
                 }
                 else
                 {
@@ -134,7 +145,7 @@ namespace test_create_password
             else
             {
                 Console.WriteLine("Please Enter a number");
-                ReadLengthFromConsole(question, validType);
+                ReadLengthFromConsole(question);
             }
         }
 
@@ -159,14 +170,23 @@ namespace test_create_password
         public void CreateRandomIntInSelectedList ()
         {
 
-            for (int i=0; i<passwordLength ; i++) 
+            if (anyValidInput)
             {
-                int tempRandomValue = random.Next(1, selectedList.Count+1);
- 
-                AddCharacterToPassword(selectedList[tempRandomValue - 1]);
+                 for (int i=0; i<passwordLength ; i++) 
+                 {
+                 int tempRandomValue = random.Next(1, selectedList.Count+1);
+
+                 AddCharacterToPassword(selectedList[tempRandomValue - 1]);
+                 }
+
+                 finalPassword = string.Join("", password);
+                 WriteFinalPasswordToConsole();
             }
-            finalPassword = string.Join("", password);
-            WriteFinalPasswordToConsole();
+            else
+            {
+                Console.WriteLine("You should select at least one of avaliable the options.");
+            }
+           
 
         }
 
@@ -203,8 +223,7 @@ namespace test_create_password
         {
             var tempNumber = random.Next(1, listName.Count);
             password.Add(listName[tempNumber- 1]);
-        }
-        
+        }   
     }
 
 }
--- test-create-password/Program.cs
+++ test-create-password/Program.cs
@@ -7,8 +7,8 @@ CreatePassword yen = new CreatePassword();
 yen.CreateRandomIntInSelectedList();
 
 // The following are extra  information about the selection of the user on the Console
-CreatePassword.selectedList.ForEach(x =>Console.Write($"{x}, "));
-Console.WriteLine($"PasswordLength: {yen.passwordLength}");
+yen.getSelectedList().ForEach(x =>Console.Write($"{x}, "));
+Console.WriteLine($"PasswordLength: {yen.getPasswordLength()}");
 
 
 
